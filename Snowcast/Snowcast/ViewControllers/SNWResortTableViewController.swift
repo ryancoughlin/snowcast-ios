237f7490e40772a0cdba4d18e2b9baa17c8d01d4
Ryan Coughlin <ryan@ryancoughlin.com>
2015-09-22 19:55:29 -0400

    Organize and add in empty table view controller for resorts

---
+++ Snowcast/Snowcast/ViewControllers/SNWResortTableViewController.swift
@@ -0,0 +1,17 @@
+import Foundation
+import UIKit
+
+class SNWResortTableViewController: UITableViewController {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSourceArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("SNWResortTableViewCell") as! UITableViewCell
+        return cell
+    }
+}
