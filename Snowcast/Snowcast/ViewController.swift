f4e2b83f7b3550e286b70ebf0cb72a3a3783cefa
Ryan Coughlin <ryan@ryancoughlin.com>
2015-09-22 19:21:38 -0400

    Add base Xcode project

---
+++ Snowcast/Snowcast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snowcast
+//
+//  Created by Ryan Coughlin on 9/22/15.
+//  Copyright © 2015 Ryan Coughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
