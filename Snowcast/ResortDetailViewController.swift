0677b67cd33dd099b71a142bbfc1ddf684cefef6
Ryan Coughlin <ryan@ryancoughlin.com>
2015-11-03 13:55:06 -0500

    Clean up scale functionality

--- Snowcast/ResortDetailViewController.swift
+++ Snowcast/ResortDetailViewController.swift
@@ -6,56 +6,29 @@ import UIKit
     @IBOutlet weak var resortNameLabel: UILabel!
     
     var resort: ResortPreview?
-    
+    let baseDepth: CGFloat = 100
     let newSnow: Float = 12
-    var baseDepth: CGFloat {
-        get {
-            return scaledBaseHeight()
-        }
-        
-        set {
-            return scaledBaseHeight(newValue)
-        }
-    }
-
-    let multiplier: CGFloat = 12
-    let screenHeight = UIScreen.mainScreen().bounds.height
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         resortNameLabel?.text = resort?.name
-        
-        baseDepth = 10
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
-        self.snowLevelsView.baseHeight.constant = baseDepth
-        print(self.snowLevelsView.baseHeight.constant)
-        self.snowLevelsView.newSnow24Height.constant = getNewSnowHeight(newSnow)
+        let heightsFromAPI = Heights.init(newSnow: 2, baseDepth: 60)
+        let scaledHeights = heightsFromAPI * scaleFactor
+        print(scaledHeights)
+
+        self.snowLevelsView.baseHeight.constant = CGFloat(scaledHeights.baseDepth)
+        self.snowLevelsView.newSnow24Height.constant = CGFloat(scaledHeights.newSnow)
 
         UIView.animateWithDuration(1, delay: 0.75, usingSpringWithDamping: 0.35, initialSpringVelocity: 0.45, options: [], animations: {
             
             self.view.layoutIfNeeded()
             
-            }, completion: nil)
-    }
-    
-    func getNewSnowHeight(newSnow: Float) -> CGFloat {
-        let scaled = newSnow
-        let foo = (scaled / 33.9674) * 100
-        print("New snow height:", scaled, foo)
-        
-        return CGFloat(foo)
-    }
-    
-    func scaledBaseHeight() -> CGFloat {
-        let scaled = baseDepth * multiplier
-        let foo = ((scaled / screenHeight) * 100)
-        print(scaled, foo)
-        
-        return foo
+        }, completion: nil)
     }
 }
---
+++ Snowcast/ScaleViewHeights.swift
@@ -0,0 +1,9 @@
+//
+//  ScaleViewHeights.swift
+//  Snowcast
+//
+//  Created by Ryan Coughlin on 11/3/15.
+//  Copyright © 2015 Ryan Coughlin. All rights reserved.
+//
+
+import Foundation
